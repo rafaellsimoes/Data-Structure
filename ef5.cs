ec7fac12e0b2219cd12b2ed54a12b4df982b746f
Rafael <48397061+rafaellsimoes@users.noreply.github.com>
2023-08-04 05:24:02 -0400

    Add files via upload

---
+++ ef5.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Globalization;
+
+class prog2 {
+    public static void Main (string[] args) {
+        Console.WriteLine("Frase");
+        string frase1 = Console.ReadLine();
+        Console.WriteLine(frase1);
+    }
+}
